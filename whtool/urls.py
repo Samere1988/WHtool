55227dd3286ab5574184225f448b47dbff374d99
Plex <samer.elgrably@gmail.com>
2026-05-12 20:58:48 -0400

    Revert "Route redesigned stats pages"
    This reverts commit 70993d175c6b5e1921a080b490538807bb1bc675.

--- whtool/urls.py
+++ whtool/urls.py
@@ -2,16 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core import stats_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('stats/', stats_views.stats_landing, name='stats'),
-    path('stats/workers/', stats_views.worker_stats, name='worker_stats'),
-    path('stats/run-sheets/', stats_views.run_sheet_stats, name='run_sheet_stats'),
-
     path('', include('core.urls')),
+
+    # ADD THIS LINE: Connects Django's built-in login/logout system
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
